2792c6c70931ebeedd9b66a480d21187b2e47a3b
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2023-10-31 08:16:01 +0900

    add Test Generator Block

--- src/main/java/constantan/mxdemon/client/ClientSetup.java
+++ src/main/java/constantan/mxdemon/client/ClientSetup.java
@@ -1,9 +1,13 @@
 package constantan.mxdemon.client;
 
+import constantan.mxdemon.common.init.ModBlocks;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 public class ClientSetup {
 
     public static void init(final FMLClientSetupEvent event) {
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.TEST_GENERATOR.get(), RenderType.translucent());
     }
 }
