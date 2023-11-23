1aab694920e42bd96c3ef955e0fd54c75b5e6503
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2023-11-23 19:54:44 +0900

    add test demon item

--- src/main/java/constantan/mxdemon/common/init/ModItems.java
+++ src/main/java/constantan/mxdemon/common/init/ModItems.java
@@ -1,6 +1,7 @@
 package constantan.mxdemon.common.init;
 
 import constantan.mxdemon.common.ModSetup;
+import constantan.mxdemon.common.item.TestDemonItem;
 import constantan.mxdemon.lib.LibMisc;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
@@ -20,4 +21,7 @@ public class ModItems {
 
     public static final RegistryObject<Item> TEST_GENERATOR = ITEMS.register("test_generator_item",
             () -> new BlockItem(ModBlocks.TEST_GENERATOR.get(), new Item.Properties().tab(ModSetup.CREATIVE_TAB)));
+
+    public static final RegistryObject<Item> TEST_DEMON = ITEMS.register("test_demon",
+            () -> new TestDemonItem(new Item.Properties().tab(ModSetup.CREATIVE_TAB)));
 }
---
+++ src/main/java/constantan/mxdemon/common/item/TestDemonItem.java
@@ -0,0 +1,10 @@
+package constantan.mxdemon.common.item;
+
+import net.minecraft.world.item.Item;
+
+public class TestDemonItem extends Item {
+
+    public TestDemonItem(Properties pProperties) {
+        super(pProperties);
+    }
+}
